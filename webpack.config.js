b5c8fa5f458f6800c98c8474e19761a7a298d35d
Thomas Kelley <TKelley05@gmail.com>
2019-06-11 16:56:45 -0700

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,62 @@
 const path = require('path')
 const webpack = require('webpack')
+const MiniCssExtraPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'public/scripts'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        resolve: {
-          extensions: ['.js', '.jsx']
+module.exports = (env) => {
+  const isProduction = env === 'production'
+  const CSSExtract = new MiniCssExtraPlugin({ filename: 'styles.css' })
+
+  return {
+    mode: 'development',
+    entry: './src/index.jsx',
+    output: {
+      path: path.resolve(__dirname, 'public/scripts'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          resolve: {
+            extensions: ['.js', '.jsx']
+          },
+          exclude: /node_modules/,
+          use: [
+            'babel-loader',
+            'eslint-loader'
+          ]
         },
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-          'eslint-loader'
-        ]
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
+        {
+          test: /\.(scss|css)$/,
+          use: [
+            {
+              loader: MiniCssExtraPlugin.loader
+            },
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true }
+            },
+            {
+              loader: 'sass-loader',
+              options: { sourceMap: true }
+            }
+          ]
+        }
+      ]
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.resolve(__dirname, 'public'),
+      publicPath: '/scripts/',
+      watchContentBase: true,
+      hot: true,
+      host: '0.0.0.0',
+      disableHostCheck: true,
+      historyApiFallback: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      CSSExtract
     ]
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-    publicPath: '/scripts/',
-    watchContentBase: true,
-    hot: true,
-    host: '0.0.0.0',
-    disableHostCheck: true,
-    historyApiFallback: true
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ]
+  }
 }
